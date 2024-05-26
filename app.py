5d2b6c55e08f2e20639780450a552c73e7e986a0
RasmusJakub <kuba.rasmus@gmail.com>
2024-05-26 13:15:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
-from flask import Flask
+import numpy as np
 
-# Create a flask
-app = Flask(__name__)
+class Perceptron:
+    def __init__(self, learning_rate=0.01, n_iters=1000):
+        self.learning_rate = learning_rate
+        self.n_iters = n_iters
+        self.activation_func = self._unit_step_func
+        self.weights = None
+        self.bias = None
 
-# Create an API end point
-@app.route('/')
-def say_hello():
-    return "Hello World"
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
 
-if __name__ == '__main__':
-    app.run() # domyślnie ustawia localhost i port 5000
-    # app.run(host='0.0.0.0', port=8000)
+        y_ = np.array([1 if i > 0 else 0 for i in y])
+
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                linear_output = np.dot(x_i, self.weights) + self.bias
+                y_predicted = self.activation_func(linear_output)
+
+                update = self.learning_rate * (y_[idx] - y_predicted)
+                self.weights += update * x_i
+                self.bias += update
+
+    def predict(self, X):
+        linear_output = np.dot(X, self.weights) + self.bias
+        y_predicted = self.activation_func(linear_output)
+        return y_predicted
+
+    def _unit_step_func(self, x):
+        return np.where(x >= 0, 1, 0)
+
+if __name__ == "__main__":
+    
+    # Example usage
+    X = np.array([[1, 1], [2, 2], [3, 3], [4, 4]])
+    y = np.array([0, 0, 1, 1])
+    p = Perceptron(learning_rate=0.01, n_iters=1000)
+    p.fit(X, y)
+    print(p.predict(np.array([[3, 3], [1, 1]])))
